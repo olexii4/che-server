ba8d0946760530df547ef9f16ebb24d4ddc3e36c
Oleksii Orel <oorel@redhat.com>
2025-12-12 08:50:39 +0200

    chore(headers): add EPL header to redeploy script
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- scripts/redeploy-che.sh
+++ scripts/redeploy-che.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+# Copyright (c) 2018-2025 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
 #
 # Simple redeploy helper for Eclipse Che on an existing cluster.
 # - Patches CheCluster to use a custom che-server image
